16faf45f0eeae323c8edb727152891f0659eb271
MonicaAgrela <monicacosta_6@hotmail.com>
2023-12-21 21:09:23 +0000

    front page done

---
+++ JS/game.js
@@ -0,0 +1,14 @@
+class Game {
+  constructor() {
+    this.start = document.getElementById("game-intro");
+    this.player1 = player1;
+    this.player2 = player2;
+    this.table = table;
+  }
+
+  start() {
+   this.start(); 
+  }
+}
+
+
